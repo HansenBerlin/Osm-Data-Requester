3217a8be66b34aa4335ccf900cbac1161fbf1643
Hannes Roever <h.roever@zertificon.com>
2023-04-05 13:19:31 +0200

    fix file path

--- Osm-Data-Requester/Program.cs
+++ Osm-Data-Requester/Program.cs
@@ -4,7 +4,7 @@ var httpClient = new HttpClient();
 var requester = new Requester(httpClient);
 var csvWriter = new CsvWriter();
 var csvReader = new CsvReader();
-string[] csvContent = csvReader.GetFirstColumnValuesFromCsv(@"data\immo_data_cleared_reduced.csv");
+string[] csvContent = csvReader.GetFirstColumnValuesFromCsv(@"data/immo_data_cleared_reduced.csv");
 var runner = new Runner(requester, csvWriter);
-await runner.Run(csvContent, 0, 100, @"newlocations.csv");
+await runner.Run(csvContent, 0, 100, @"data/newlocations.csv");
 
