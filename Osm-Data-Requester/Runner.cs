a9b7b3a7a8a152eab3f62f8ea0224139fe0570ef
Hannes Roever <h.roever@zertificon.com>
2023-04-05 14:24:04 +0200

    fix index

--- Osm-Data-Requester/Runner.cs
+++ Osm-Data-Requester/Runner.cs
@@ -16,11 +16,11 @@ public class Runner
         _writer.InitCsv(outputPath);
         int length = csvContent.Length;
         endIndex = endIndex == -1 ? length : endIndex;
-        Console.WriteLine($"Starting requests beginning at index {startIndex + 1} " +
-                          $"till {endIndex + 1}. Length of file: {length - 1}");
+        Console.WriteLine($"Starting requests every {throttle}ms, beginning at index {startIndex + 1} " +
+                          $"till {endIndex + 1}. Length of file: {length - 1}.");
         for (int i = 1 + startIndex; i < endIndex + 1; i++)
         {
-            if (i == length)
+            if (i == length + 1)
             {
                 break;
             }
